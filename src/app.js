7da497dff370d50fca8acd3ada320159cae0d23b
Mahad <itzmahad01@gmail.com>
2026-07-05 20:40:54 +0500

    Contact Add

--- src/app.js
+++ src/app.js
@@ -5,12 +5,24 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://product-fe-seven.vercel.app/",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+app.use(express.json());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Expense Tracker API Running...");
 });
+
 
 app.use("/api", expenseRoutes);
 
